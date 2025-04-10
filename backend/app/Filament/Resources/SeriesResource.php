d562dbb75c36c5bc28f8622ed88d6c7382f3290e
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-04-10 13:50:06 +0300

    Added series to the dashboard

---
+++ backend/app/Filament/Resources/SeriesResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SeriesResource\Pages;
+use App\Filament\Resources\SeriesResource\RelationManagers;
+use App\Models\Series;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SeriesResource extends Resource
+{
+    protected static ?string $model = Series::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Series Information')
+                    ->description('View and manage all the details for this series')
+                    ->aside()
+                    ->schema([
+                        Forms\Components\SpatieMediaLibraryFileUpload::make('series')
+                            ->collection(Series::MEDIA_COLLECTION_COVER)
+                            ->alignCenter()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('title')
+                            ->helperText('Glamorous title for your series')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('description')
+                            ->helperText('Provide a clear and concise description for the viewers')
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('cover')
+                    ->collection(Series::MEDIA_COLLECTION_COVER)
+                    ->width(518 / 10)
+                    ->height(768 / 10),
+
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('description')
+                    ->limit(60)
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->slideOver(),
+
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSeries::route('/'),
+        ];
+    }
+}
