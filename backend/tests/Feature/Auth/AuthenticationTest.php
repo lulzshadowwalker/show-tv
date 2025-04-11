b90094ed92a438ef2899bec2d85e517b29212021
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-04-11 12:14:20 +0300

    Fixed broken tests

--- backend/tests/Feature/Auth/AuthenticationTest.php
+++ backend/tests/Feature/Auth/AuthenticationTest.php
@@ -30,7 +30,7 @@ class AuthenticationTest extends TestCase
 
         $response
             ->assertHasNoErrors()
-            ->assertRedirect(route('dashboard', absolute: false));
+            ->assertRedirect(route('home.index', absolute: false));
 
         $this->assertAuthenticated();
     }
--- backend/tests/Feature/Auth/RegistrationTest.php
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Auth;
 
 use App\Livewire\Auth\Register;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Testing\File;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -20,16 +22,24 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
+        $avatar = File::image('avatar.png', 100, 100);
+
         $response = Livewire::test(Register::class)
             ->set('name', 'Test User')
             ->set('email', 'test@example.com')
             ->set('password', 'password')
             ->set('password_confirmation', 'password')
+            ->set('avatar', $avatar)
             ->call('register');
 
         $response
             ->assertHasNoErrors()
-            ->assertRedirect(route('dashboard', absolute: false));
+            ->assertRedirect(route('home.index', absolute: false));
+
+        $this->assertDatabaseCount('users', 1);
+
+        $user = User::first();
+        $this->assertNotNull($user->avatarFile);
 
         $this->assertAuthenticated();
     }
